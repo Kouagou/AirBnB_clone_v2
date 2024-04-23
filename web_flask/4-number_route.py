b12b5e77c9a4f249c76dfef69b0eb30db8290463
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-23 09:07:27 -0700

    Task 4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application. """
+from markupsafe import escape
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Return Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_(text):
+    """Display C followed by <text>. """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_is_(text="is cool"):
+    """Display Python followed by <text>. """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """Display <n> is a number if true. """
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
