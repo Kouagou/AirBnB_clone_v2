46d69742a8e2629a9d5760d9ab8a03e8a8657d99
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-24 16:12:27 -0700

    Fix tests

--- models/state.py
+++ models/state.py
@@ -17,12 +17,12 @@ class State(BaseModel, Base):
         cities = relationship("City",
                               backref="state",
                               cascade="all, delete, delete-orphan")
-    """else:
+    else:
         @property
-        def cities(self): """
+        def cities(self):
             """Return the list of City instances corresponding to this state"""
-            """ from models import storage
+            from models import storage
             my_cities = storage.all(City)
             state_city = [cc for cc in my_cities.values()
                           if cc.state_id == self.id]
-            return state_city """
+            return state_city
