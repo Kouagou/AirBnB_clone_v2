d1748a0d1cbb78b567dfd0e85f03d8e6098eeccb
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-25 05:12:22 -0700

    Task 8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ A script that starts a Flask web application. """
-from markupsafe import escape
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -11,7 +10,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def get_states_list():
     """Display all states """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    #states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
 
